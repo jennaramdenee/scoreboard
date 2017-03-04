02dc26f4a0a0072231cf748c5af551d37d586771
jennaramdenee <jenna_ramdenee@hotmail.co.uk>
2017-03-04 13:33:08 +0000

    Refactored Counter into its own component

--- app.js
+++ app.js
@@ -22,11 +22,7 @@ function Player(props) {
         {props.name}
       </div>
       <div className="player-score">
-      </div>
-      <div className="counter">
-        <button className="counter-action decrement"> - </button>
-        <div className="counter-score"> {props.score} </div>
-        <button className="counter-action increment"> + </button>
+        <Counter score={props.score} />
       </div>
     </div>
   )
@@ -39,8 +35,16 @@ Player.PropTypes = {
 
 function Counter(props) {
   return (
-
+    <div className="counter">
+    <button className="counter-action decrement"> - </button>
+    <div className="counter-score"> {props.score} </div>
+    <button className="counter-action increment"> + </button>
+    </div>
   )
+}
+
+Counter.PropTypes = {
+  score: React.PropTypes.number.isRequired
 }
 
 function Application(props){
