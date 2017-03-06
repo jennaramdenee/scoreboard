7b85ca02dbb816b87dae2785a9c81a76f058b4b0
jennaramdenee <jenna_ramdenee@hotmail.co.uk>
2017-03-06 21:11:48 +0000

    Add 'stats' class to header, to show statistics of the game'

--- app.js
+++ app.js
@@ -23,13 +23,15 @@ var PLAYERS = [
 function Header(props){
   return (
     <div className="header">
+      <Stats players={ props.players }/>
       <h1> { props.title } </h1>
     </div>
   )
 }
 
 Header.propTypes = {
-  title: React.PropTypes.string.isRequired
+  title: React.PropTypes.string.isRequired,
+  players: React.PropTypes.array.isRequired
 }
 
 function Player(props) {
@@ -66,6 +68,33 @@ Counter.propTypes = {
   onChange: React.PropTypes.func.isRequired
 }
 
+function Stats(props){
+  var totalPlayers = props.players.length
+  var totalPoints = props.players.reduce(function(total, player){
+    return total + player.score
+  }, 0)
+
+  return (
+    <table className="stats">
+      <tbody>
+        <tr>
+          <td>Players:</td>
+          <td>{totalPlayers}</td>
+        </tr>
+        <tr>
+          <td>Total Points</td>
+          <td>{totalPoints}</td>
+        </tr>
+      </tbody>
+    </table>
+  )
+}
+
+Stats.propTypes = {
+  players: React.PropTypes.array.isRequired,
+
+}
+
 var Application = React.createClass({
   propTypes: {
     title: React.PropTypes.string,
@@ -96,7 +125,7 @@ var Application = React.createClass({
   render: function(){
     return (
       <div className="scoreboard">
-      <Header title={this.props.title} />
+      <Header title={this.props.title} players={this.state.players} />
       <div className="players">
         {this.state.players.map(function(player, index){
           return (
